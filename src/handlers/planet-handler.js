0d416101438470b875cbc0ae48951969e24021b1
Pedro <p.mariano95@yahoo.com.br>
2021-01-10 12:49:44 -0300

    feat: solving test 'Creating a planet - should return "missing name field on body"'

---
+++ src/handlers/planet-handler.js
@@ -0,0 +1,40 @@
+class PlanetHandler {
+    static async create(request, response) {
+        const responseObject = {
+            message: '',
+            data: null,
+            meta: {
+                api_status: 'stable'
+            }
+        }
+
+        try {
+            const { body } = request
+
+            PlanetHandler.verifyRequiredFields(body)
+
+            response.status(201)
+
+        } catch (error) {
+            response.status(400)
+
+            responseObject.message = error.message
+        }
+
+        return response.send(responseObject)
+    }
+
+    static verifyRequiredFields(body) {
+        const { name, climate, ground } = body
+
+        PlanetHandler.verifyField(name, 'missing name field on body')
+    }
+
+    static verifyField(field, errorMessage) {
+        if (!field || field === null) {
+            throw new Error(errorMessage)
+        }
+    }
+}
+
+module.exports = PlanetHandler
